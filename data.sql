38d54a551878c591aa33d4479bd809175eae5b38
ali <exit>
2022-08-11 18:51:30 +0530

    updaed data in all files as per requirements

--- data.sql
+++ data.sql
@@ -1,7 +1,7 @@
-INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (1,'Agumon', 'Feb 3, 2020', 10.23, '1', 0);
-INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (2,'Gabumon', 'Nov 15, 2018', 8, '1', 2);
-INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (3,'Pikachu', 'Jan 7, 2021', 15.04, '0', 1);
-INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (4,'Devimon', 'May 12, 2017', 11, '1', 5);
+INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (1,'Agumon', 'Feb 3, 2020', 10.23, TRUE, 0);
+INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (2,'Gabumon', 'Nov 15, 2018', 8, TRUE, 2);
+INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (3,'Pikachu', 'Jan 7, 2021', 15.04, FALSE, 1);
+INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (4,'Devimon', 'May 12, 2017', 11, TRUE, 5);
 INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (5,'Charmander', '08/02/2020', 0, FALSE, -11.0);
 INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (6,'Plantmon', '15/11/2021', 2, TRUE, -5.7);
 INSERT INTO animals (id,name, date_of_birth, weight_kg, neutered, escape_attempts) VALUES (7,'Squirtle', '02/04/1993', 3, FALSE, -12.13);
